4056c6d51bda5edac9a2e6fa9aa1679ab03e78f1
Nghiep <nghiep@Nghieps-MacBook-Pro.local>
2024-10-20 02:40:16 +0900

    fix web url

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -2,7 +2,10 @@ export const fetchFromApi = async (
   endpoint: string,
   params: Record<string, string> = {}
 ) => {
-  const url = new URL(`/api/pokemon`, window.location.origin); // Call Next.js API route
+  // Construct the base URL for API routes
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'; // Fallback for local development
+
+  const url = new URL(`/api/pokemon`, baseUrl); // Use baseUrl instead of window.location.origin
   url.searchParams.append('endpoint', endpoint);
   Object.keys(params).forEach((key) =>
     url.searchParams.append(key, params[key])
