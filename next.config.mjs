1c6043eb4002c90d5af8bf452a0fbc98dc0eec4b
Nghiep <nghiep@Nghieps-MacBook-Pro.local>
2024-11-25 22:02:42 +0900

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.pokemontcg.io'],
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/pokemon-tcg-ez-search/' : '',
-  basePath: isProd ? '/pokemon-tcg-ez-search' : '',
-  output: 'export'
+  basePath: '/pokemon-tcg-ez-search',
+  output: 'export',
 };
 
 export default nextConfig;
